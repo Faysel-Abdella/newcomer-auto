d3384546dec449cf2b672e7b8c784fd8ca9e3890
Faysel Abdella <fayselcode@gmail.com>
2025-02-01 15:54:03 +0300

    feat: remove unused SVG files and update Home component layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,20 @@ import Image from "next/image";
 // font-[family-name:var(--font-geist-sans)]
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <main className="relative h-screen bg-[#F3F1F4]">
+      <div className="absolute top-0 left-0 w-full h-full">
+        <Image
+          src={"/backgrounds/hero.svg"}
+          width={1512}
+          height={969}
+          alt="America"
+          className="w-full h-full object-cover"
+          priority
+        />
+      </div>
+
+      <section></section>
+    </main>
+  );
 }
